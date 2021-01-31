706a839154603bb75eb47a3fb133d2fba2d4853e
Berk <berk.turan2@isik.edu.tr>
2021-01-31 21:45:20 +0300

    Initial Commit

---
+++ LoodosMovieChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosMovieChallange
+//
+//  Created by Berk Turan on 1/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
